313a5f620fb167ae2686e7976b03969bd4a9638b
Student <Student@Train11-4-PC207.control.ad.sit.kmutt.ac.th>
2025-07-24 12:42:19 +0700

    class04

---
+++ class04_datastructure.py
@@ -0,0 +1,40 @@
+# x = ["Sun","Tik"]
+
+# x[1] = "Vava"
+
+# print(x)
+
+# x.append("Ton")
+# x.pop(1)
+# print(x)
+# num =[1,2,3,4,5,6,7,8,9,10]
+# for number in (num):
+#     if (number %2 == 0):
+#         print("Even: ",number)
+#     else:
+#         print("Odd: ",number)
+# x = {"name":"sun",
+#       "sid":681305}
+#  print(x["name"],x["sid"])
+#  x["score"]=100
+#  x[]
+# students = [
+#     {"name":"sun","sid":671305,"score":100},
+#     {"name":"tik","sid":671305,"score":1000}
+# ]
+# for students in students:
+#     print(students["name"],students["score"])
+
+# students = [
+#     {"name":"tik","id": 123,"score":80},
+#     {"name":"tok","id": 146,"score":90},
+#     {"name":"zok","id": 765,"score":40}
+# ]
+# for students in students:
+#     if(students["score"] >= 90):
+#         students["score"] = "A"
+#     elif(students["score"] >= 80):
+#         students["score"] = "B"
+#     else:
+#         students["score"] = "B"
+#     print(students)
---
+++ class04_function.py
@@ -0,0 +1,51 @@
+# # def sum(a,b):
+# #     return a+b
+# # print(sum(19,20))
+
+# def add(num1,num2):
+#     result = num1 + num2
+#     return result
+ 
+# def minus(num1,num2):
+#     result = num1 - num2
+#     return result
+# def multiple(num1,num2):
+#     result = num1 * num2
+#     return result
+# def divide(num1,num2):
+#     result = num1 / num2
+#     return result
+# def is_even(num):
+#     result = num%2
+#     if (result == 0):
+#         print("is even")
+#     else :
+#         print("is odd")
+    
+# def main():
+#     num1 = int(input("กรอกเลขตัวที่ 1: "))
+#     num2 = int(input("กรอกเลขตัวที่ 2: "))
+#     print(" + - * / เอาอันไหนน้อง")
+#     print("[1] +")
+#     print("[2] -")
+#     print("[3] *")
+#     print("[4] /")
+#     operation = input("เลือกสักอัน: ")
+#     # ตรงนี้ให้น้องๆเขียน condition (if-else) ว่าถ้ามันตรงกับเลขนั้นในเมนูจะให้ใช้ฟังก์ชั่นอะไร
+#     if (operation == "1"):
+#         result = add(num1,num2)
+#         print("ผลบวกคือ:",result)
+    
+#     elif (operation == "2"):
+#         result = minus(num1,num2)
+#         print("ผลลบคือ:",result)
+
+#     elif (operation == "3"):
+#         result = multiple(num1,num2)
+#         print("ผลคูณคือ:",result)
+ 
+#     # เรียก is_even เพื่อเช็กว่าผลลัพที่ได้มันเป็นเลขคู่มั้ย
+#     print(is_even(result))
+ 
+# main()
+
